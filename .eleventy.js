803c52cebfe2db79f24c29102f260480d3801e6e
smoyamedina <smoyamedina@outlook.com>
2021-08-26 00:00:44 -0400

    Partializing progress.

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,8 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output: "public"
+            output: "public",
+            includes: "partials"
         }
     };
 }
