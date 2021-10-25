0ed4ad96afac7110ca1979ec0f13f9367c66a615
smoyamedina <smoyamedina@outlook.com>
2021-10-25 00:56:27 -0400

    passthrough js files, test loco

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,18 @@
 module.exports = function(eleventyConfig) {
-    
+
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy('./src/copy');
-    
+    eleventyConfig.addPassthroughCopy('./src/scripts');
+
     return {
         dir: {
             input: "src",
             output: "public",
             includes: "partials",
             data: "copy"
-        }
+        },
+        htmlTemplateEngine: "pug"
     };
 }
