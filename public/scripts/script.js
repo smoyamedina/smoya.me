c3567b0863f0862cd9cdd1e7081a222aacb92ed0
smoyamedina <smoyamedina@outlook.com>
2021-11-09 23:37:32 -0500

    more layout work

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,10 @@
 gsap.registerPlugin(CSSRulePlugin);
 
 //smooth scroll
-const locoScroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
+// const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true
+// });
 
 document.onreadystatechange = function () {
     // because loco inits before all heights are rendered completely. need to refresh it.
