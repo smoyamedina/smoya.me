acd868b8c6ac702949a669d3d934b3005660c6b9
smoyamedina <smoyamedina@outlook.com>
2021-12-28 23:13:53 -0500

    null check for slider

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,32 +5,34 @@ function initMain () {
     // ▶ ionut daniel - much appreciated, you have no idea - draggable horizontal scrollable div ◀
 
     const slider = document.querySelector('.extras-slide-gall');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+    if ( slider !== null) {
+        let isDown = false;
+        let startX;
+        let scrollLeft;
 
-    slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-    });
-    slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-    });
-    slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-    });
-    slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    slider.scrollLeft = scrollLeft - walk;
-    console.log(walk);
-    });
+        slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+        });
+        slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+        });
+        slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        console.log(walk);
+        });
+    }
 
     // const scrollerWrapper = '.container'
 
